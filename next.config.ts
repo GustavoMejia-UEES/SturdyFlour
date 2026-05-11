900cb5fe067a2ff5fe9e5e26dba861185d045ab0
GustavoMejia-UEES <gmejiar@uees.edu.ec>
2026-05-10 23:41:34 -0500

    chore: refine production build scripts and dev platform shim

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default async function config() {
+  // Automatically load D1, R2, etc bindings when running in dev mode!
+  if (process.env.NODE_ENV === 'development') {
+    const { setupDevPlatform } = await import('@cloudflare/next-on-pages/next-dev');
+    await setupDevPlatform();
+  }
+  return nextConfig;
+}
